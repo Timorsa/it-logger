451bf9516a10410a1f87733f0ce008881928768a
Timor Safadi <timor.saf@gmail.com>
2020-03-22 16:37:08 +0200

    before redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 import SearchBar from './components/layout/SearchBar';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 
 function App() {
 	useEffect(() => {
@@ -15,6 +19,12 @@ function App() {
 	return (
 		<Fragment>
 			<SearchBar />
+			<div className='container'>
+				<AddBtn />
+				<EditLogModal />
+				<AddLogModal />
+				<Logs />
+			</div>
 		</Fragment>
 	);
 }
