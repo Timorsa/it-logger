9a1ccb7bebcfa512be3e6c588415dbe9f5ea2da7
Timor Safadi <timor.saf@gmail.com>
2020-03-22 14:02:18 +0200

    before redux

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+import SearchBar from './components/layout/SearchBar';
+
+function App() {
+	useEffect(() => {
+		// initializes Materialize JS
+		M.AutoInit();
+	});
+
+	return (
+		<Fragment>
+			<SearchBar />
+		</Fragment>
+	);
+}
+
+export default App;
