1132074b07b497761a9d7223de5a3c78d8cc646a
Timor Safadi <timor.saf@gmail.com>
2020-03-23 14:22:34 +0200

    before tech

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, ADD_LOG, LOGS_ERROR } from './types';
+import {
+	GET_LOGS,
+	SET_LOADING,
+	ADD_LOG,
+	DELETE_LOG,
+	LOGS_ERROR,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_LOG
+} from './types';
 
 // get logs from server
 export const getLogs = () => async dispatch => {
@@ -40,6 +49,66 @@ export const addLog = log => async dispatch => {
 			payload: err.response.data
 		});
 	}
+};
+
+// delete logs from server
+export const deleteLog = id => async dispatch => {
+	try {
+		setLoading();
+		await fetch(`/logs/${id}`, {
+			method: 'DELETE'
+		});
+
+		dispatch({
+			type: DELETE_LOG,
+			payload: id
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// update log on server
+export const updateLog = log => async dispatch => {
+	try {
+		setLoading();
+		const res = await fetch(`/logs/${log.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(log),
+			header: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		const data = await res.json();
+		dispatch({
+			type: UPDATE_LOG,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+};
+
+// Set Curr log
+export const setCurrent = log => {
+	return {
+		type: SET_CURRENT,
+		payload: log
+	};
+};
+
+// clear Curr log
+export const clearCurrent = () => {
+	return {
+		type: CLEAR_CURRENT
+	};
 };
 
 export const setLoading = () => {
